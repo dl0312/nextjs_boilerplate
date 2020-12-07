ed282a26c0a79f9d16ce568ad66cde55045ca5b2
geon lee <geon.lee@navercorp.com>
2020-12-07 22:49:55 +0900

    🔧 miner change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
