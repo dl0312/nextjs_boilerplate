4c9371e9731a58f1d3148a461e62e56ed39907f1
geon lee <geon.lee@navercorp.com>
2020-12-20 01:03:05 +0900

    ✨ useDarkMode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import ThemeToggleSwitch from 'components/ThemeToggleSwitch'
+import styled from 'styles/themed-components'
+
+const Title = styled.div`
+  color: ${({ theme: { linkText } }) => linkText};
+`
+
 function Home() {
-  return <div>home</div>
+  return (
+    <div>
+      <Title>타이틀</Title>
+      <ThemeToggleSwitch />
+    </div>
+  )
 }
 
 export default Home
