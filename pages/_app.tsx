556f2796a71b354534b67ecce517d843c5d3c31e
geon lee <geon.lee@navercorp.com>
2021-01-04 16:37:41 +0900

    ✨ darkmode with antd, no flash/flicker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,32 @@
-import { ThemeType } from 'interfaces/theme'
-import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { GlobalStyle } from 'styles/global-styles'
-import { themes } from 'styles/theme'
-import styled, { ThemeProvider } from 'styles/themed-components'
-import useDarkMode from 'use-dark-mode'
-
-import 'node_modules/antd/dist/antd.min.css'
 import { useState, useEffect } from 'react'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  min-height: 100vh;
-`
+import { ThemeType } from 'interfaces/theme'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { ThemeSwitcherProvider } from 'react-css-theme-switcher'
+import { GlobalStyle } from 'styles/global-styles'
+import { themes } from 'styles/theme'
+import { ThemeProvider } from 'styles/themed-components'
+import useDarkMode from 'use-dark-mode'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 function App({ Component, pageProps }: AppProps) {
   const darkMode = useDarkMode(false)
+
   const [mounted, setMounted] = useState(false)
   useEffect(() => {
     setMounted(true)
   }, [])
 
+  const themeMap = {
+    [ThemeType.LIGHT]: 'antd.min.css',
+    [ThemeType.DARK]: 'antd.dark.css',
+  }
+
   const body = (
     <ThemeProvider theme={themes[darkMode.value ? ThemeType.DARK : ThemeType.LIGHT]}>
-      <Container>
+      <ThemeSwitcherProvider defaultTheme={darkMode.value ? ThemeType.DARK : ThemeType.LIGHT} themeMap={themeMap}>
         <Component {...pageProps} />
-      </Container>
+      </ThemeSwitcherProvider>
     </ThemeProvider>
   )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
+import Layout from 'components/Layout'
 import ThemeToggleSwitch from 'components/ThemeToggleSwitch'
 import styled from 'styled-components'
 
 const Title = styled.div`
   font-size: 2rem;
-  font-weight: bolder;
   margin: 1rem;
 `
 
 function Home() {
   return (
-    <>
+    <Layout>
       <Title>Next.js Boilerplate</Title>
       <ThemeToggleSwitch />
-    </>
+    </Layout>
   )
 }
 
